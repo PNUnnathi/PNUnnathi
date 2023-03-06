6a70b99a422dc4262714050c05d68d7f20a88a5f
PNUnnathi <112872176+PNUnnathi@users.noreply.github.com>
2023-03-06 19:01:55 +0530

    Create problem3.c

---
+++ problem3.c
@@ -0,0 +1,22 @@
+/*MinMax in an array
+
+Use divide and Conquer approach to find the maximum and minimum element present in an array.
+
+Input Format
+
+First line: 'N' indicating number of elements
+Second line: N numbers separated by space
+
+Output Format
+
+Min element separated by space with maximum element
+
+Testcase 1
+Input
+
+5
+50 40 10 90 30
+Output
+
+10 90*/
+
